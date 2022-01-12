de99a3d8346665b570ae030000b0c927161d5945
alcenirdev08 <cenirdecarvalho@gmail.com>
2022-01-11 22:26:27 -0300

    first commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+      Gato gato = new Gato();
+      System.out.println(gato);
+
+        /*int a = 3;
+        int b = 5;
+        System.out.println("hello word!" + (a+b)); */
+    }
+}
